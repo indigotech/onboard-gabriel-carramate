b401bd1c1b8fc5ce494c8458ab0c22461b07d59c
Gabriel Carramate <gabriel.carramate@taqtile.com.br>
2021-08-11 16:44:03 -0300

    create users list

--- src/Pages/HomePage.tsx
+++ src/Pages/HomePage.tsx
@@ -1,5 +1,32 @@
 import React from 'react';
 
+const list = [
+  {
+    id: 0,
+    username: 'Gabriel',
+    useremail: 'gabriel.carramate@taqtile.com.br',
+  },
+  {
+    id: 1,
+    username: 'admin',
+    useremail: 'admin@taqtile.com.br',
+  },
+  {
+    id: 2,
+    username: 'Teste',
+    useremail: 'teste@taqtile.com.br',
+  },
+];
+
 export function HomePage() {
-  return <p>Home Page</p>;
+  return (
+    <ul>
+      <h3>Lista de usuários</h3>
+    {list.map(item => (
+      <li key={item.id}>
+        <div>name: {item.username} email: {item.useremail}</div>
+      </li>
+    ))}
+  </ul>
+  );
 }
