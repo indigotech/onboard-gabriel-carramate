c9703927710f857d94aa94fee7756affe6bd46e4
Gabriel Carramate <gabriel.carramate@taqtile.com.br>
2021-08-17 17:25:59 -0300

    improve code readability

--- src/Components/ErrorComponent.tsx
+++ src/Components/ErrorComponent.tsx
@@ -1,5 +1,5 @@
-import { errorStyle } from "../Utils/styles";
+import { errorStyle } from '../Utils/styles';
 
-export function ErrorComponent({error} : {error : any}) {
-    return <>{error && <p style={errorStyle}>{error.message}</p>}</>;
+export function ErrorComponent({ error }: { error: any }) {
+  return <>{error && <p style={errorStyle}>{error.message}</p>}</>;
 }
